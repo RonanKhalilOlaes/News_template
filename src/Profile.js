c4af60cb8da355629d104dffb4f4a58c4775080a
RonanKhalilOlaes <113484040+RonanKhalilOlaes@users.noreply.github.com>
2023-09-18 09:04:03 +0400

    Initial commit

---
+++ src/Profile.js
@@ -0,0 +1,11 @@
+/*
+ Profile ((
+   content: 'My Profile attribute content',
+   id: " ";
+   children:
+ ))
+*/
+export function Profile(props) {
+  const content = "My other awesome profile";
+  return <p> (props.content) </p>;
+}
